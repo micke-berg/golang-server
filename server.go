351d07290e635a895501d114285d71df336d0e4d
Micke Berg <micke.berg.dev@gmail.com>
2020-01-28 18:00:06 +0100

    Removed unnecessary code

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "fmt"
     "log"
     "net/http"
 )
@@ -9,15 +8,8 @@ import (
 func main() {
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Content-Type", "text/html; charset=utf-8")
-        w.Header().Set("Access-Control-Allow-Origin", "*")
         http.ServeFile(w, r, r.URL.Path[1:])
     })
 
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hi")
-    })
-
     log.Fatal(http.ListenAndServe(":8081", nil))
-
 }
